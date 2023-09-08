70bb6147bfdace8c5ef5c7bc7a02befa8918fc9e
Badsha Hossain <badshab454@gmail.com>
2023-09-08 15:55:07 +0600

    todos wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,17 @@ const App = () => {
           <input type="text" className="form-control" placeholder="Enter Your Todo" name="text"/>
           <button type="button" className="btn"><FaPlusCircle className="icon"/></button>
         </form>
+        <div className="todos_wrapper">
+          <div className="todo_list">
+            <h3 className="todo_title">Todo</h3>
+          </div>
+          <div className="todo_list">
+            <h3 className="todo_title">Doing</h3>
+          </div>
+          <div className="todo_list">
+            <h3 className="todo_title">Done</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
