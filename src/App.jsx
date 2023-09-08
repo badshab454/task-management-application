a12b9c104c76fa7e309fa5aa70b9779050ddf0bb
Badsha Hossain <badshab454@gmail.com>
2023-09-08 15:34:30 +0600

    react icon installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <div className="container">
+        <h3 title>Task Management Application</h3>
+        <form className="form_todo">
+          <input type="text" className="form-control" onChange={(e) => setInput(e.target.value)} placeholder="Enter Your Todo" name="text"/>
+          <button type="button" className="btn" onClick={() => addTodo()}><FaPlusCircle className="icon"/></button>
+        </form>
+      </div>
+    </div>
   )
 }
 
