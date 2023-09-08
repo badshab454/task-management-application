98e534d055aa02e71fdf9a45fcf3022ea4baca3a
Badsha Hossain <badshab454@gmail.com>
2023-09-08 15:37:31 +0600

    FaPlusCircle icon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+
+import './App.css';
+import {FaPlusCircle} from 'react-icons/fa';
+
 const App = () => {
   return (
     <div className="App">
       <div className="container">
         <h3 title>Task Management Application</h3>
         <form className="form_todo">
-          <input type="text" className="form-control" onChange={(e) => setInput(e.target.value)} placeholder="Enter Your Todo" name="text"/>
-          <button type="button" className="btn" onClick={() => addTodo()}><FaPlusCircle className="icon"/></button>
+          <input type="text" className="form-control" placeholder="Enter Your Todo" name="text"/>
+          <button type="button" className="btn"><FaPlusCircle className="icon"/></button>
         </form>
       </div>
     </div>
