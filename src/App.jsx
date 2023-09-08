872a52487eae2c237dbe837985f3f45c5cbb6633
Badsha Hossain <badshab454@gmail.com>
2023-09-08 15:45:16 +0600

    title and form

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <div className="container">
-        <h3 title>Task Management Application</h3>
+        <h3 className="title">Task Management Application</h3>
         <form className="form_todo">
           <input type="text" className="form-control" placeholder="Enter Your Todo" name="text"/>
           <button type="button" className="btn"><FaPlusCircle className="icon"/></button>
